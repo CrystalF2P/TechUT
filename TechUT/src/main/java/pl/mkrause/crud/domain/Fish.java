6f70d5c22cdd2b4b72ecbbdf98fa6ab6cc681dd5
CrystalF2P <sneer@poczta.onet.eu>
2018-10-24 17:48:37 +0200

    Add files via upload

---
+++ TechUT/src/main/java/pl/mkrause/crud/domain/Fish.java
@@ -0,0 +1,61 @@
+package pl.mkrause.crud.domain;
+
+import java.sql.Date;
+
+public class Fish {
+	
+	private long id;
+	private String gatunek;
+	private Date dataZlowienia;
+	private double waga;
+
+	
+	public Fish() {
+	}
+	
+	
+	
+	public Fish(String gatunek, Date dataZlowienia, float waga) {
+		super();
+		this.gatunek = gatunek;
+		this.dataZlowienia = dataZlowienia;
+		this.waga = waga;	
+	}
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	public String getGatunek() {
+		return gatunek;
+	}
+	public void setGatunek(String gatunek) {
+		this.gatunek = gatunek;
+	}
+	
+	
+	public Date getDataZlowienia() {
+		return dataZlowienia;
+	}
+	public void setDataZlowienia(Date dataZlowienia) {
+		this.dataZlowienia = dataZlowienia;
+	}
+	
+	
+	public double getWaga() {
+		return waga;
+	}
+	public void setWaga(double waga) {
+		this.waga = waga;
+	}
+	
+	
+	
+	
+}
---
+++ TechUT/src/main/java/pl/mkrause/crud/service/Run.java
@@ -0,0 +1,14 @@
+package pl.mkrause.crud.service;
+
+import pl.mkrause.crud.domain.Fish;
+
+public class Run {
+		
+		public static void main(String[] args) {
+			
+			Fish fish = new Fish();
+			
+			Crud test = new Crud;
+			
+		}
+}
